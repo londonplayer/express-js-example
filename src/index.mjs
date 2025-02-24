c399c74ad84d68a0ff28843ed38e80d2a74aacd1
londonplayer <jvgerhardt@gmail.com>
2025-02-24 16:01:01 -0300

    feat: endpoints

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,51 @@
 import express from "express";
 
+// mocks
+import { users, products } from "./utils/mock.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
+	console.log(`Running on port ${PORT}`);
 });
 
-app.get('/', (request, response) => {
-    response.send({ message: 'Hello World' });
-})
+app.get("/api/", (request, response) => {
+	// request is what comes from the client: headers, body, etc.
+	// response is what we send back to the client: json, html, etc.
+
+	// .send() sends a response back to the client
+	// .status() sets the status code of the response
+	response.status(200).send({ message: "Hello World" });
+});
+
+app.get("/api/users", (request, response) => {
+	response.status(200).send(users);
+});
+
+app.get("/api/users/:id", (request, response) => {
+	const { id } = request.params;
+	const user = users.find((user) => user.id === parseInt(id));
+
+	if (!user) {
+		response.status(404).send({ message: "User not found" });
+	} else {
+		response.status(200).send(user);
+	}
+});
+
+app.get("/api/products", (request, response) => {
+	response.status(200).send(products);
+});
+
+app.get("/api/products/:id", (request, response) => {
+	const { id } = request.params;
+	const product = products.find((product) => product.id === parseInt(id));
+
+	if (!product) {
+		response.status(404).send({ message: "Product not found" });
+	} else {
+		response.status(200).send(product);
+	}
+});
---
+++ src/utils/mock.js
@@ -0,0 +1,13 @@
+const users = [
+	{ id: 1, name: "John Doe" },
+	{ id: 2, name: "Jane Doe" },
+	{ id: 3, name: "Alice" },
+];
+
+const products = [
+	{ id: 1, name: "Apple" },
+	{ id: 2, name: "Banana" },
+	{ id: 3, name: "Orange" },
+];
+
+export { users, products };
